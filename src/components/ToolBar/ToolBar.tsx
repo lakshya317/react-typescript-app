c29635a2dafbbe6eacc4d678f47f84065a5b7aee
lakshya317 <lakshya.sharma@shell.com>
2022-09-11 09:20:43 +0530

    Added Support for taller phones

--- src/components/ToolBar/ToolBar.tsx
+++ src/components/ToolBar/ToolBar.tsx
@@ -3,13 +3,20 @@ import { ThemeToggle } from '../ThemeToggle/ThemeToggle';
 import Shell_Logo from '../../assets/images/Shell_Logo.png';
 import './ToolBar.scss';
 
-export default function TooolBar() {
+const TooolBar: React.FC<{}> = () => {
     return (
         <div className="toolbar">
-            <div className="shell-logo-container">
+            <div className="shell-logo-container" onClick={() => {
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth"
+                });
+            }}>
                 <img className="shell-logo" src={Shell_Logo} alt="Shell Logo" />
             </div>
             <ThemeToggle />
         </div>
     );
 }
+
+export default TooolBar;
